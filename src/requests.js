2d30b7c0a504480772ae71159d4b9ede40e8c494
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-22 18:17:04 -0600

    handled api key

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "560d7e2f4ada6c654ef91a32bf36a482";
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US` ,
